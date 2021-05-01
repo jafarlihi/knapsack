81978fbaa222217a3d4fd77c20eb9898a202d501
Hikmat Jafarli <jafarlihi@gmail.com>
2021-05-01 14:30:12 +0400

    Add more parser tests

--- src/test/java/com/mobiquity/packer/ParserTest.java
+++ src/test/java/com/mobiquity/packer/ParserTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
 
@@ -26,5 +27,35 @@ public class ParserTest {
         assertEquals(resultPackage.getItems().size(), 6);
         assertEquals(resultPackage.getItems().get(0).getPrice(), 45);
         assertEquals(resultPackage.getItems().get(0).getWeight(), 53.38f);
+    }
+
+    @Test
+    public void testParsingWithInvalidMaxWeight() {
+        List<String> content = new ArrayList<>(){{
+            add("81.55 : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)");
+        }};
+
+        Parser parser = new Parser(content);
+        assertThrows(ParseException.class, parser::parse);
+    }
+
+    @Test
+    public void testParsingWithInvalidDelimiter() {
+        List<String> content = new ArrayList<>(){{
+            add("81 , (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)");
+        }};
+
+        Parser parser = new Parser(content);
+        assertThrows(ParseException.class, parser::parse);
+    }
+
+    @Test
+    public void testParsingWithInvalidItemString() {
+        List<String> content = new ArrayList<>(){{
+            add("81 : (1,53.38,€45,55) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)");
+        }};
+
+        Parser parser = new Parser(content);
+        assertThrows(ParseException.class, parser::parse);
     }
 }
